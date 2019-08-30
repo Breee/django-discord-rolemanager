a23eb441524e1e8d8c8ffc430784b861ca55ea80
Breee <loeffler.uni@gmail.com>
2019-08-30 21:45:21 +0200

    clean up, use donator model

--- pogobackend/donations/management/commands/update_users.py
+++ pogobackend/donations/management/commands/update_users.py
@@ -1,21 +1,25 @@
 from django.core.management.base import BaseCommand
 from pogobackend.settings import BOT_TOKEN
 from donations.bot import RoleBot
-from donations.models import Donation
+from donations.models import Donator, GuildToRoleRelation
 from allauth.socialaccount.models import SocialAccount
-from datetime import datetime,timedelta
 from django.db.models import Q
-
-guild_to_roles = {
-    409418083632152577 : [616562233475989514]
-}
 
 class Command(BaseCommand):
     help = 'update users'
 
     def handle(self, *args, **kwargs):
-        donators = Donation.objects.filter(Q(monthly_paid=True) | Q(precious=True)).values_list('user__user_id',flat=True)
+        # get donators
+        donators = Donator.objects.filter(Q(monthly_paid=True) | Q(precious=True)).values_list('user__user_id',flat=True)
         members = SocialAccount.objects.filter(user__in=donators)
+        # get guild_to_role mapping
+        guild_to_roles = dict()
+        donator_roles = GuildToRoleRelation.objects.all()
+        for obj in donator_roles:
+            if obj.guild.guild_id not in guild_to_roles:
+                guild_to_roles[obj.guild.guild_id] = [obj.role.role_id]
+            else:
+                guild_to_roles[obj.guild.guild_id].append(obj.role.role_id)
         discord_members = []
         for mem in members:
             if mem.extra_data:
