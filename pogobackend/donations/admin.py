8d26de572ef3ed7605d1418d6998b36b71fb18b4
Breee <loeffler.uni@gmail.com>
2019-08-30 21:45:54 +0200

    admin models

--- pogobackend/donations/admin.py
+++ pogobackend/donations/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from donations.models import Donation
+from donations.models import Donator, Donation, AllowedDiscordServer, DiscordRole,DiscordGuild,GuildToRoleRelation
 # Register your models here.
 
+admin.site.register(Donator)
 admin.site.register(Donation)
+admin.site.register(AllowedDiscordServer)
+admin.site.register(DiscordRole)
+admin.site.register(DiscordGuild)
+admin.site.register(GuildToRoleRelation)
