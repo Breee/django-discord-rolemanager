370cb0310283475e4345f950f6e7d4eb41b0ed4f
Breee <loeffler.uni@gmail.com>
2019-09-01 23:12:44 +0200

    fixed an issue where aggregation was none because the queryset was none

--- pogobackend/donations/signals.py
+++ pogobackend/donations/signals.py
@@ -7,19 +7,20 @@ from django.contrib.auth import logout
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models import Sum
-from datetime import datetime
+from django.utils import timezone
 
 # method for updating
 @receiver(post_save, sender=Donation, dispatch_uid="update_donation")
 def update_donation(sender, **kwargs):
     donation = kwargs.get('instance')
     donator = donation.donator
-    balance = Donation.objects.filter(donator=donator, completed=True).aggregate(Sum('amount'))['amount__sum'] - donator.paid
-    Donator.objects.filter(user=donator.user).update(balance=balance, last_payment=datetime.now())
+    summed_donations = Donation.objects.filter(donator=donator, completed=True).aggregate(Sum('amount'))['amount__sum']
+    balance = (summed_donations if summed_donations is not None else 0) - donator.paid
+    Donator.objects.filter(user=donator.user).update(balance=balance, last_payment=timezone.now())
 
 @receiver(post_save, sender=Donator, dispatch_uid="update_donator")
 def update_donator(sender, **kwargs):
     donator = kwargs.get('instance')
-    balance = Donation.objects.filter(donator=donator, completed=True).aggregate(Sum('amount'))[
-                  'amount__sum'] - donator.paid
+    summed_donations = Donation.objects.filter(donator=donator, completed=True).aggregate(Sum('amount'))['amount__sum']
+    balance = (summed_donations if summed_donations is not None else 0) - donator.paid
     Donator.objects.filter(user=donator.user).update(balance=balance)
