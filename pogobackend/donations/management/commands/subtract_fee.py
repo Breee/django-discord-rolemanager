48e6e3a33327fbba7cb6463c025169b9e2c6e997
Breee <loeffler.uni@gmail.com>
2019-09-03 23:22:51 +0200

    some switches for fee substraction

--- pogobackend/donations/management/commands/subtract_fee.py
+++ pogobackend/donations/management/commands/subtract_fee.py
@@ -9,17 +9,20 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         # Positional arguments are standalone name
         parser.add_argument('-f', '--force', default=False, action='store_true')
+        parser.add_argument('-p', '--pay', default=False, action='store_true')
+        parser.add_argument('-d', '--days', default=False, action='store_true')
+        parser.add_argument('-m', '--month', default=False, action='store_true')
 
 
     def handle(self, *args, **kwargs):
         donator_queryset = Donator.objects.all()
         for donator in donator_queryset:
-            print(donator.balance >= donator.fee)
-            print(donator.days_until_payment <= 0)
-            print(donator.monthly_paid)
             if (donator.balance >= donator.fee and donator.days_until_payment <= 0 and not donator.monthly_paid) or kwargs.get('force'):
-                donator.paid = donator.paid + donator.fee
-                donator.days_until_payment = 30
-                donator.monthly_paid = True
+                if kwargs.get('pay'):
+                    donator.paid = donator.paid + donator.fee
+                if kwargs.get('days'):
+                    donator.days_until_payment = 30
+                if kwargs.get('month'):
+                    donator.monthly_paid = True
                 donator.last_payment = timezone.now()
                 donator.save()
