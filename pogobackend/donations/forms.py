5d6d13b01753b57d47d9376f033221f0cbfcd862
Breee <loeffler.uni@gmail.com>
2019-09-03 23:21:59 +0200

    Settings form

--- pogobackend/donations/forms.py
+++ pogobackend/donations/forms.py
@@ -6,3 +6,5 @@ class DonateForm(forms.Form):
     amount = forms.FloatField(label='Amount',required=True, widget=forms.NumberInput(attrs={'class' : 'form-control'}))
     note = forms.CharField(label='Note', max_length=128, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 
+class SettingsForm(forms.Form):
+    autopay = forms.BooleanField(label='Autopay', required=False)
