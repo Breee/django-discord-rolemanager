2936c805586d6234868c645d821836b2edccf3bc
Breee <loeffler.uni@gmail.com>
2019-09-03 23:22:35 +0200

    subtract day now sets monthly_paid to False if 0 days are reached.

--- pogobackend/donations/management/commands/subtract_day.py
+++ pogobackend/donations/management/commands/subtract_day.py
@@ -6,7 +6,6 @@ from django.db.models import F
 class Command(BaseCommand):
     help = 'subtract a day from each donator.'
 
-
     def handle(self, *args, **kwargs):
         Donator.objects.all().update(days_until_payment=F('days_until_payment')-1)
-
+        Donator.objects.filter(days_until_payment__lte=0).update(monthly_paid=False)
