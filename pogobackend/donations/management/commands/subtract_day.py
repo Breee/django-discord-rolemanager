217726a8513661fd85836431f447096a18ae0ae8
Breee <loeffler.uni@gmail.com>
2019-10-02 00:45:37 +0200

    Update subtract_day.py

--- pogobackend/donations/management/commands/subtract_day.py
+++ pogobackend/donations/management/commands/subtract_day.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from donations.models import Donator
-from django.db.models import F
+from django.db.models import F,Q
 
 
 class Command(BaseCommand):
@@ -8,4 +8,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         Donator.objects.filter(days_until_payment__gt=0).update(days_until_payment=F('days_until_payment')-1)
-        Donator.objects.filter(days_until_payment__lte=0).update(monthly_paid=False)
+        Donator.objects.filter(Q(days_until_payment__lte=0) & Q(precious=False)).update(monthly_paid=False, updated=False)
